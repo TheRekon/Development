fb29efcef5abfff75e1ff01d34287c7996bc1cd4
TheRekon <migopotam@gmail.com>
2017-12-17 16:49:32 +0500

    rtr

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -15,8 +15,22 @@ http://wiki.darkrp.com/index.php/DarkRP:CustomJobFields
 
 Add your custom jobs under the following line:
 ---------------------------------------------------------------------------]]
-
-
+TEAM_SBA = DarkRP.createJob("MTF", {
+	color = Color(30, 100, 160, 255),
+	model = {"
+			
+			},
+	description = [[Элитные подразделения, собранные из сотрудников разных учреждений Фонда и мобилизованные для применения в случае конкретных угроз или ситуаций, сложность которых превышает оперативный потенциал или опыт обычных полевых сотрудников.]],
+	weapons = {arx-160},
+	command = "",
+	max = 999,
+	salary = 0,
+	admin = 0,
+	vote = false,
+	hasLicense = false,
+	candemote = false,
+	category = "МОГ",
+})
 
 --[[---------------------------------------------------------------------------
 Define which team joining players spawn into and what team you change to if demoted
@@ -25,13 +39,8 @@ GAMEMODE.DefaultTeam = TEAM_CITIZEN
 --[[---------------------------------------------------------------------------
 Define which teams belong to civil protection
 Civil protection can set warrants, make people wanted and do some other police related things
+
 ---------------------------------------------------------------------------]]
-GAMEMODE.CivilProtection = {
-    [TEAM_POLICE] = true,
-    [TEAM_CHIEF] = true,
-    [TEAM_MAYOR] = true,
-}
 --[[---------------------------------------------------------------------------
 Jobs that are hitmen (enables the hitman menu)
 ---------------------------------------------------------------------------]]
-DarkRP.addHitmanTeam(TEAM_MOB)
